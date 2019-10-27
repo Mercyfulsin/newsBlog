2fc05c24389e4ffed818409c90a3aac99cdfd01f
Andres Ruiz-Velediaz <aruiz858@gmail.com>
2019-10-26 23:08:26 -0700

    Can add comments and saves to db.

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -19,33 +19,40 @@ $(document).ready(() => {
     $(".addcomment").click(function (e) {
         e.preventDefault();
         const id = $(this).attr("data-id");
-        const group = $(`<div class="input-group mt-3">`);
-        const textbox = $(`<textarea class="form-control comment" placeholder="Enter comment here..." aria-label="Enter comment here..." aria-describedby="button-addon2">`);
-        const buttonAlignment = $(`<div class="input-group-append">`);
-        const button = $(`<button class="btn btn-outline-secondary submitcomment" data-id=${id} type="button" id="button-addon2">Submit</button>`);
-        button.click(function (e) {
-            e.preventDefault();
-            const id = $(this).attr("data-id");
-            console.log("Content: ", $(".comment").val());
-            $.ajax({
-                method: "POST",
-                url: `/articles/${id}`,
-                data: { body: "test" },
-            }).then(function (dbArticle) {
-                console.log(dbArticle);
-            }).catch(function (err) {
+        $(this).hide();
+        const ref = this;
+        const bodyref = $(this).parent().children("div.card-text");
+        $.ajax({
+            method: "GET",
+            url: `/articles/${id}`
+        })
+            .then(function (reply) {
+                const textbox = $(`<textarea class="form-control comment" placeholder="Enter comment here..." aria-label="Enter comment here..." aria-describedby="button-addon2">`);
+                reply.comment ? textbox.val(reply.comment.body) : '';
+                const group = $(`<div class="input-group mt-3">`);
+                const buttonAlignment = $(`<div class="input-group-append">`);
+                const button = $(`<button class="btn btn-outline-secondary submitcomment" data-id=${id} type="button" id="button-addon2">Submit</button>`);
+                button.on("click", function (e) {
+                    e.preventDefault();
+                    $.ajax({
+                        method: "POST",
+                        url: `/articles/${id}`,
+                        data: { body: $(".comment").val() },
+                    }).then(function (dbArticle) {
+                        console.log(dbArticle);
+                    }).catch(function (err) {
+                        console.log(err);
+                    });
+                    $(this).parent().parent().parent().children("a.addcomment").show();
+                    $(this).parent().parent().remove();
+                });
+                buttonAlignment.append(button);
+                group.append(textbox, buttonAlignment);
+                $(ref).parent().append(group);
+            })
+            .catch(function (err) {
                 console.log(err);
             });
-            const commentBtn = $(`<a href="#" data-id="${id}" class="btn btn-warning addcomment">Add Comment</a>`);
-            $(this).parent().children("div.card-body").append(commentBtn);
-            $(this).parent().children("div.input-group").remove();
-        });
-        buttonAlignment.append(button);
-        group.append(textbox, buttonAlignment);
-        $(this).parent().append(group);
-        $(this).remove();
-        console.log("done");
     });
-
 
 });
